47e5e713a2c4d3ab9c1e4ea113d1eb3e36e6d351
Hannah Gulle <hancgulle@gmail.com>
2018-04-17 22:07:01 -0400

    Repository Cleaned and Ready

--- PthreadSolvers/driver.f90
+++ PthreadSolvers/driver.f90
@@ -121,7 +121,7 @@ do i=1, NDIM
    residual = max(residual, abs(vecx(i)-dble(i)))
 enddo
 
-mflops2  = (2.0)*dble(NDIM)**3/ (wall_end-wall_start) / 1.0e6
+mflops2  = (2.0/3.0)*dble(NDIM)**3/ (wall_end-wall_start) / 1.0e6
 
 
 print *, NDIM, residual, cpu_end-cpu_start, wall_end-wall_start,  mflops2, nthreads
